f0abd2d329da6f22c1a91ed93a67ec0fa23e3616
LukaoModesto <lucasoliveiractt@outlook.com>
2026-06-09 14:23:10 -0300

    feat: improve about section animations

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
 
-    entries.forEach((entry) =>{
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
 
-    });
+        });
+    },
+    {
+        threshold: 0.5
+    }
+);
 
+const hiddenElements = document.querySelectorAll(".fade-up");
+
+hiddenElements.forEach((el) => {
+    observer.observe(el);
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+const aboutLink = document.getElementById("about-link");
+
+const aboutTitle = document.getElementById("about-title");
+
+aboutLink.addEventListener("click", () => {
+
+    setTimeout(() => {
+
+        aboutTitle.classList.remove("about-click-animation");
+
+        void aboutTitle.offsetWidth;
+
+        aboutTitle.classList.add("about-click-animation");
+
+    }, 600);
+
+});
